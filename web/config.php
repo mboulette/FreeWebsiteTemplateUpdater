d1fa96470b40024210d35a468b813cacc956155d
jofrenette <jofrenette@idealtechnology.net>
2013-12-05 11:39:17 -0500

    Patch Image Home

--- web/config.php
+++ web/config.php
@@ -2,5 +2,5 @@
 
 //define("REPO_PATH", dirname(__FILE__).'/repo/TestFreeWebsiteBatch');
 define("REPO_PATH", dirname(__FILE__).'/repo/FreeWebsiteTemplate');
-define("SCRIPT_PATH", 'scripts/script.php');
+define("SCRIPT_PATH", 'scripts/copy_home_img.php');
 ?>
