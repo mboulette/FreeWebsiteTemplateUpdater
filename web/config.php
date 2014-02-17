5bba078d2cb7265e09eae34ec4dca100e9411adf
jofrenette <jofrenette@idealtechnology.net>
2014-02-17 09:19:15 -0500

    Fix target blank on external link in the main menu

--- web/config.php
+++ web/config.php
@@ -2,5 +2,5 @@
 
 //define("REPO_PATH", dirname(__FILE__).'/repo/TestFreeWebsiteBatch');
 define("REPO_PATH", dirname(__FILE__).'/repo/FreeWebsiteTemplate');
-define("SCRIPT_PATH", 'scripts/copy_refresh_img.php');
+define("SCRIPT_PATH", 'scripts/target-blank-menu.php');
 ?>
