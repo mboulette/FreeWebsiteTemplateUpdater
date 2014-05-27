054fd80e5d128164f81b4f0b27abdbf5198f31e2
mboulette <mboulette@idealtechnology.net>
2014-05-27 13:52:39 -0400

    Patch du menu vertical

--- web/config.php
+++ web/config.php
@@ -2,5 +2,5 @@
 
 //define("REPO_PATH", dirname(__FILE__).'/repo/TestFreeWebsiteBatch');
 define("REPO_PATH", dirname(__FILE__).'/repo/FreeWebsiteTemplate');
-define("SCRIPT_PATH", 'scripts/switch-zip-states.php');
+define("SCRIPT_PATH", 'scripts/target-blank-menu-vertical.php');
 ?>
